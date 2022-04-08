14375d0d29f94d28390337c8a25ef2e04fefdadc
FelipeEnne <felipeenne@gmail.com>
2022-04-08 09:31:09 -0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# Microservide NodeJS Kafka
---
+++ certification/src/index.js
@@ -0,0 +1,39 @@
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  brokers: ["localhost:9092"],
+  clientId: "certificate",
+});
+
+const topic = "issue-certificate";
+const consumer = kafka.consumer({ groupId: "certificate-group" });
+
+const producer = kafka.producer();
+
+async function run() {
+  await consumer.connect();
+  await producer.connect();
+  await consumer.subscribe({ topic });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`;
+      console.log(`- ${prefix} ${message.key}#${message.value}`);
+
+      const payload = message.value;
+
+      // setTimeout(() => {
+      producer.send({
+        topic: "certification-response",
+        messages: [
+          {
+            value: `Testado ${payload}`,
+          },
+        ],
+      });
+      // }, 3000);
+    },
+  });
+}
+
+run().catch(console.error);
